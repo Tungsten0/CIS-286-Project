4504aa85d1bde5624a34b09828ef8e2a5b42d0d5
Marco Lee-Shi <115311708+Tungsten0@users.noreply.github.com>
2023-05-04 23:35:07 -0600

    Update program-detail.php
    design change

--- program-detail.php
+++ program-detail.php
@@ -19,9 +19,28 @@ mysqli_stmt_bind_param($stmt, "s", $program_id);
 mysqli_stmt_execute($stmt);
 $data = mysqli_stmt_get_result($stmt);
 
+
+// Initialize the 2D array
+$course_info = array();
+
 // Iterate through each tuple and save courses in array
 while ($row = mysqli_fetch_assoc($data)) {
-    $course_id[] = $row['course_id'];
+    $course_id = $row['course_id'];
+
+    // Get course information from the courses table
+    $course_stmt = mysqli_prepare($con,
+        "SELECT *
+        FROM courses
+        WHERE id = ?");
+    mysqli_stmt_bind_param($course_stmt, "s", $course_id);
+    mysqli_stmt_execute($course_stmt);
+    $course_data = mysqli_stmt_get_result($course_stmt);
+    $course_info[$course_id] = mysqli_fetch_assoc($course_data);
+}
+
+
+if ($course_id == null) {
+    echo 'courses table is empty';
 }
 
 if($data == null) {
@@ -61,17 +80,56 @@ $department = $result["department"];
 
         <h4>Credits: <span style="font-weight: 400;"> <?php echo $credits; ?></span></h4>
         <h4>Department: <span style="font-weight: 400;"> <?php echo $department; ?></span></h4>
-        <h5>Courses:</h5>
-        <span style="font-weight: 400;">
-            <ul>
-                <?php foreach ($course_id as $id) { ?>
-                    <li><a href="course.php?id=<?php echo $id; ?>"><?php echo $id; ?></a></li>
-                <?php } ?>
-            </ul>
-        </span>
+        <h5>Professional Core Courses:</h5>
+        
+        <div class="parent-container" style="overflow: auto;">
 
+            <?php foreach ($course_info as $course_id => $course) { ?>
+
+                <a href="course.php?id=<?php echo $course_id ?>">
+                <div class="card square-card card-hover" 
+                style="margin-left: 20px; margin-right: 20px; margin-top: 20px; margin-bottom: 20px; 
+                width: 200px; height: 200px; display: block; float: left; border-radius: 0; color: white;">
+                    <div class="card-body">
+                    <p class="card-text">#<?php echo $course['department']; ?></p>
+                    <p class="card-text"><?php echo $course['id']; ?></p>
+                    <h6 class="card-title"><?php echo $course['title']; ?></h6>
+                    <p class="card-text">Credits: <?php echo $course['credits']; ?></p>
+                    </div>
+                </div>
+                </a>
+
+            <?php } ?>
+
+            <div class="clearfix"></div>
+
+        </div>
+            
     </div>
 
+    <style>
+        .card-hover {
+      background-color: #200174;
+      transition: all 0.3s ease-in;
+    }
+    
+    .card-hover:hover {
+      background-color: #2f7ad5;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      opacity: 0.9;
+      transform: scale(1.05);
+    }
+
+    .card-hover .card-title,
+    .card-hover .card-text {
+      color: #fff;
+    }
+        .clearfix {
+            clear: both;
+        }
+    </style>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.min.js"></script>
 </main>
 
 <?php
