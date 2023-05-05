4c7329a6cdcf1bf8b49bbb67e2b1e2adda22a8d4
MushroomAlmond <121523821+MushroomAlmond@users.noreply.github.com>
2023-05-05 00:46:32 -0600

    about us update
    added information

--- about.php
+++ about.php
@@ -7,64 +7,71 @@
         
         <!--top text and description-->
         <div class="container text-center">
-            <h1 style="color: #d0d7f7;">Our CIS-286 Project</h1>
-            <h2 style="color: #d0d7f7;">__</h2>
             <br>
-            <h5 style="color: #d0d7f7;">Description of project</h5>
-            <h5 style="color: #d0d7f7;">why</h5>
-            <h5 style="color: #d0d7f7;">when</h5>
-            <h5 style="color: #d0d7f7;">mein</h5>
-        </div>
+            <h1 style="color: #d0d7f7;">Our CIS-286 Project</h1>
+            <h2 style="color: #d0d7f7;">--------------</h2>
+            <br>
+            <h5 style="color: #d0d7f7;">This assessment affords you the opportunity to put 
+            into practice the concepts and technologies learned in class by tackling a "real-world" 
+            project. Your task is to design and develop a web application using PHP that caters to
+            the academic programs of SJCJC. Upon project completion, you will have created a 
 
+        </div>
+<br>
         <!--info cards of people-->
         <div class="container text-center">
+            <br>
+        <h1 style="color: #d0d7f7;"> Meet The Team: </h1>
+        <h2 style="color: #d0d7f7;">--------------</h2>
             <div class="row">
                 <div class="col-md-4">
-                    <div class="card" style="background-color: #41258f; width: 18rem;">
+                    <div class="card" style="background-color: #41258f; width: 20rem; height: 32rem;">
                         <img class="card-img-top" src="media/img1.png" alt="person 2" height="200px">
                         <div class="card-body" style="color: #d0d7f7;">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h3 class="card-title">Leanna Guitterez</h3>
+                            <h5 class="card-title" style="color: #B5BDE1">Head Researcher </h5>
+                            <h5> ======= </h5>
+                            <p class="card-text">Is responsible for overseeing the reasearch , and leading the research and development of the project. She identified and analyzed the information for this project and compiled and present her findings in an organized manner. </p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="card" style="background-color: #41258f; width: 18rem;">
+                    <div class="card" style="background-color: #41258f; width: 20rem; height: 32rem;">
                         <img class="card-img-top" src="media/img1.png" alt="person 2" height="200px">
-                        <div class="card-body" style="color: #d0d7f7;">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div class="card-body" style="color: #B5BDE1;">
+                            <h3 class="card-title">Marco Lee-Shi</h3>
+                            <h5 class="card-title">Head Programer</h5>
+                            <h5> ======= </h5>
+
+                            <p class="card-text">Is responsible for leading and managing the team of the project. He oversaw the development process, ensure that the code is of high quality, and ensure that the project meets the necessary technical specifications.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="card" style="background-color: #41258f; width: 18rem;">
+                    <div class="card" style="background-color: #41258f; width: 20rem; height: 32rem;">
                         <img class="card-img-top" src="media/img1.png" alt="person 2" height="200px">
-                        <div class="card-body" style="color: #d0d7f7;">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        
-                    </div>
-                </div>
+                        <div class="card-body" style="color: #B5BDE1;">
+                            <h3 class="card-title">Amali Teck</h3>
+                            <h5 class="card-title">UI Designer</h5>
+                            <h5> ======= </h5>
+                            <p class="card-text"> Is responsible for designing the visual layout and interaction of the project. She creates user-centered deigns that are both visually appealing and intuitive to use.</p>
+                        </div>                        
             </div>
         </div>
-
-        
-        
-
-        <h1>this is about us page</h1>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+</div>
+    <br>
+    <br>
+        <div class="col-md-4">
+                    <div class="card" style="background-color: #41258f; width: 75rem;">
+                        <img class="card-img-top" src="media/img1.png" alt="person 2" height="500px">
+                        <div class="card-body" style="color: #d0d7f7;">
+                            <h4 class="card-title">- Group Contribution -</h4>
+                            <p class="card-text">The group members collaborated to validate the data displayed on the website and stored in the databases. They ensured that the data was accurate, consistent, and up-to-date. They also conducted several tests to validate the security controls and ensure that the system was robust and efficient, with high user-friendliness. The group members worked together to analyze the test results and implement necessary changes to improve the system's overall performance and usability.</p>
+                        </div>
+                    </div>
+                </div>
+<br>
+<br>
         
     </main>
 
