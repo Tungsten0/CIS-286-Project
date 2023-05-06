8af87adc9cf5d62d15ee757fe91b3040d6be0154
MushroomAlmond <121523821+MushroomAlmond@users.noreply.github.com>
2023-05-06 00:06:56 -0600

    about us help

--- about.php
+++ about.php
@@ -2,78 +2,156 @@
   require "header.php";
 ?>
 
+    <style>
 
-    <main style="background-color: #200174;">
+        .aboutus{
+            padding:0; 
+            margin:0;
+            box-sizing: border-box;
+            text-decoration: none;
+        }
         
+
+        .team-members{
+            display:flex; 
+            justify-content: space-around;
+            align-items: center; 
+            min-height: 50v; 
+            position: relative;
+            background-color: rgb(13,31,79);
+            color: #f2f2f2;
+            padding: 3.5rem 0;
+
+        }
+
+        .members{
+            display: grid; 
+            grid-template-column: 1fr 1fr; 
+            grid-template-rows: auto auto;
+            gap: 20px; 
+            padding: 2rem;
+        }
+
+        .members .member-card{
+            padding:1rem 2.5rem;
+            background-color: #fff;
+            border-radius: 10px; 
+            display: flex; 
+            align-items: center; 
+            justify-content: center; 
+            flex-direction:column;
+            position: relative; 
+            box-shadow:5px 10px 10px rgba(0,0,0,0.6);
+            transition: 0.5s;
+
+        }
+
+        .member-card img{
+            border-radius:50%;
+            object-fit: cover;
+        }
+
+        .member-card h2{
+            text-align: center;
+            color:#222;
+            font-weight:400;
+            font-size: 1.2rem;
+            margin: 1rem;
+        }
+
+        .member-card h5{
+            text-align: center;
+            color:#222;
+            font-weight:400;
+            font-size: 1.2rem;
+            background-color: #fdfdfd;
+            padding: 5px 10px;
+            border-radius: 30px;
+            margin:5px;
+        }
+
+        .content{
+            max-width: 700px;
+        }
+
+        .content h1{
+            font-size: 3.5rem;
+            color: #fff;
+            margin:1.3rem;
+        }
+
+        .content h3{
+            font-size: 24px;
+            color: #fff;
+            margin:1.3rem;
+        }
+
+        .content p{
+            color: #41258f;
+            font-size: 18px;
+            margin-bottom: 1rem;
+        }
+    
+    </style>
+    <main style="background-color: #200174;">
+
+    <div class="aboutus">
         <!--top text and description-->
-        <div class="container text-center">
-            <br>
-            <h1 style="color: #d0d7f7;">Our CIS-286 Project</h1>
-            <h2 style="color: #d0d7f7;">--------------</h2>
-            <br>
-            <h5 style="color: #d0d7f7;">This assessment affords you the opportunity to put 
-            into practice the concepts and technologies learned in class by tackling a "real-world" 
-            project. Your task is to design and develop a web application using PHP that caters to
-            the academic programs of SJCJC. Upon project completion, you will have created a 
-
-        </div>
-<br>
-        <!--info cards of people-->
-        <div class="container text-center">
-            <br>
-        <h1 style="color: #d0d7f7;"> Meet The Team: </h1>
-        <h2 style="color: #d0d7f7;">--------------</h2>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card" style="background-color: #41258f; width: 20rem; height: 32rem;">
-                        <img class="card-img-top" src="media/img1.png" alt="person 2" height="200px">
-                        <div class="card-body" style="color: #d0d7f7;">
-                            <h3 class="card-title">Leanna Guitterez</h3>
-                            <h5 class="card-title" style="color: #B5BDE1">Head Researcher </h5>
-                            <h5> ======= </h5>
-                            <p class="card-text">Is responsible for overseeing the reasearch , and leading the research and development of the project. She identified and analyzed the information for this project and compiled and present her findings in an organized manner. </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card" style="background-color: #41258f; width: 20rem; height: 32rem;">
-                        <img class="card-img-top" src="media/img1.png" alt="person 2" height="200px">
-                        <div class="card-body" style="color: #B5BDE1;">
-                            <h3 class="card-title">Marco Lee-Shi</h3>
-                            <h5 class="card-title">Head Programer</h5>
-                            <h5> ======= </h5>
-
-                            <p class="card-text">Is responsible for leading and managing the team of the project. He oversaw the development process, ensure that the code is of high quality, and ensure that the project meets the necessary technical specifications.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card" style="background-color: #41258f; width: 20rem; height: 32rem;">
-                        <img class="card-img-top" src="media/img1.png" alt="person 2" height="200px">
-                        <div class="card-body" style="color: #B5BDE1;">
-                            <h3 class="card-title">Amali Teck</h3>
-                            <h5 class="card-title">UI Designer</h5>
-                            <h5> ======= </h5>
-                            <p class="card-text"> Is responsible for designing the visual layout and interaction of the project. She creates user-centered deigns that are both visually appealing and intuitive to use.</p>
-                        </div>                        
+    <div class ="team-members">
+        <div class = "members">
+            <div class="member-card">
+                <img src="media/Head Programmer.png" alt="Head-Programmer Icon";
+                <h2>Marco Lee-Shi</h2>
+                <h5>Head Programmer </h5>
             </div>
         </div>
-</div>
+    </div>
+
+    <div class ="team-members">
+        <div class = "members">
+            <div class="member-card">
+                <img src="media/Head Researcher.png" alt="Head-Researcher Icon";
+                <h2>Leanna Guitterez</h2>
+                <h5>Head Researcher </h5>
+            </div>
+        </div>
+    </div>
+
+    <div class ="team-members">
+        <div class = "members">
+            <div class="member-card">
+                <img src="media/designer.png" alt="UI-Designer Icon";
+                <h2>Marco Lee-Shi</h2>
+                <h5>UI Designer </h5>
+            </div>
+        </div>
+    </div>
+
+        <div class="content">
+            <h1>CIS -286 Project</h1>
+            <p>This assessment affords you the opportunity to put 
+            into practice the concepts and technologies learned in class by tackling a "real-world" 
+            project. The task was to design and develop a web application using PHP that caters to
+            the academic programs of SJCJC. </p>
+            
+            <h3>Individual Contribution</h3>
+            <p >Head Programmer: Is responsible for leading and managing the team of the project. He oversaw the development process, ensure that the code is of high quality, and ensure that the project meets the necessary technical specifications.</p>
+
+            <p>Head Researcher: Is responsible for overseeing the reasearch , and leading the research and development of the project. She identified and analyzed the information for this project and compiled and present her findings in an organized manner. </p>
+
+            <p >UI Designer: Is responsible for designing the visual layout and interaction of the project. She creates user-centered deigns that are both visually appealing and intuitive to use.</p>
+
+
+
+            <h3>Group Contribution</h3>
+            <p>The group members collaborated to validate the data displayed on the website and stored in the databases. They ensured that the data was accurate, consistent, and up-to-date. They also conducted several tests to validate the security controls and ensure that the system was robust and efficient, with high user-friendliness. The group members worked together to analyze the test results and implement necessary changes to improve the system's overall performance and usability.</p>
+        </div>
+
+
+    </div>
+
     <br>
-    <br>
-        <div class="col-md-4">
-                    <div class="card" style="background-color: #41258f; width: 75rem;">
-                        <img class="card-img-top" src="media/img1.png" alt="person 2" height="500px">
-                        <div class="card-body" style="color: #d0d7f7;">
-                            <h4 class="card-title">- Group Contribution -</h4>
-                            <p class="card-text">The group members collaborated to validate the data displayed on the website and stored in the databases. They ensured that the data was accurate, consistent, and up-to-date. They also conducted several tests to validate the security controls and ensure that the system was robust and efficient, with high user-friendliness. The group members worked together to analyze the test results and implement necessary changes to improve the system's overall performance and usability.</p>
-                        </div>
-                    </div>
-                </div>
-<br>
-<br>
-        
-    </main>
+</main>
 
 
 <?php
